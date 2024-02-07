f9851d51707dfd93742a7cce18f5283acc3425ae
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-02-07 14:34:29 -0500

    Update Page9.java

--- Page9.java
+++ Page9.java
@@ -2,13 +2,13 @@
 *Project: Demo for Comments
 *Programmer: Maverick
 *Date: February, 2024
-*Program Name: Comments.java
+*Program Name: Page9.java
 ******************/
 public class Page9
 {
     public static void main(String[] args)
     {
-        System.out.println("Hi");
-        System.out.println("LabOneA");
+        System.out.println("Hi"); //message to user
+        System.out.println("LabOneA");//assignment
     }
 }
