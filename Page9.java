c4931884e2fb4906f2fe93e4a9d994497cd1dc6c
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-02-07 14:33:26 -0500

    Create Page9.java

---
+++ Page9.java
@@ -0,0 +1,14 @@
+/******************
+*Project: Demo for Comments
+*Programmer: Maverick
+*Date: February, 2024
+*Program Name: Comments.java
+******************/
+public class Page9
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hi");
+        System.out.println("LabOneA");
+    }
+}
